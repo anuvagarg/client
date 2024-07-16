6434ec45f3bb5c1711cd0dad92e7f71ee5159193
anuva <500083146@stu.upes.ac.in>
2024-07-16 06:00:21 +0530

    Update index.js

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -5,7 +5,14 @@ const Logout = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    localStorage.setItem("user", "");
+    // Clear user data from local storage
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    
+    // Optionally, clear cookies or other storage methods if used
+    document.cookie = 'token=; Max-Age=0; path=/; domain=master--ditto-client.netlify.app;';
+
+    // Redirect to the login page
     navigate("/login");
   }, [navigate]);
 
